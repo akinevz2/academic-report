761f5051dbca1101f57e5b908a7de2f4f432dd92
Kirill Nevzorov <kn253@sussex.ac.uk>
2025-11-17 10:59:56 +0000

    remove project files from Template repository

--- schematics/src/main/java/uk/ac/sussex/kn253/workbench/resources/WorksheetResource.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.sussex.kn253.workbench.resources;
-
-public class WorksheetResource {
-    
-}
--- schematics/src/main/java/uk/ac/sussex/kn253/workbench/services/PromptService.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.sussex.kn253.workbench.services;
-
-public class PromptService {
-    
-}
